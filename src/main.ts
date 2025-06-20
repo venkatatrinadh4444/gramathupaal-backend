8295291640f7b6b7d6361f86a1f920db2ba69938
venkata trinadh <venkatatrinadh4444@gmail.com>
2025-06-20 20:12:52 +0530

    added the frontend origin

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser())
   app.enableCors({
-    origin:"http://localhost:3000",
+    origin:["https://gramathupaal.vercel.app","http://localhost:3000"],
     credentials:true
   })
   const config=new DocumentBuilder()
